580ffe8eb4c2dd8ac234b686cded934a7579137e
Iretomi <114882292+Iretomi@users.noreply.github.com>
2023-04-25 00:04:02 +0200

    Initial commit

---
+++ App.tsx
@@ -0,0 +1,146 @@
+import React from 'react';
+import './style.css';
+import {
+  Layout,
+  ElementsType,
+  TextTile,
+  ImageTile,
+  ButtonTile,
+  HorizontalSplitter,
+  VerticalSplitter,
+  Action,
+  Color,
+} from './model';
+
+let App: React.FC = () => {
+  let layout: Layout = {
+    rootElement: {
+      type: 'verticalSplitter',
+      elements: [
+        {
+          type: 'textTile',
+          elementKey: 'textTile1',
+          title: 'I Love Yogurt',
+          text: 'Yogurt is a delicious and healthy treat that I enjoy!',
+          color: 'dark',
+        },
+        {
+          type: 'imageTile',
+          elementKey: 'imageTile1',
+          title: 'my-yogo',
+          source:
+            'http://4.bp.blogspot.com/-e1Jz6sKoYK4/UzmmPT4xuQI/AAAAAAAABDw/uCm_kkog4HQ/s1600/parfait+10jpg',
+        },
+
+        {
+          type: 'horizontalSplitter',
+          elementKey: 'middle',
+          elements: [
+            {
+              type: 'textTile',
+              elementKey: 'textTile2',
+              title: 'Pot of Life',
+              color: 'mid',
+            },
+          ],
+        },
+        {
+          type: 'verticalSplitter',
+          elementKey: 'right',
+          elements: [
+            {
+              type: 'textTile',
+              elementKey: 'textTile3',
+              title: 'Yogurt Examples',
+              color: 'mid',
+            },
+            {
+              type: 'textTile',
+              elementKey: 'textTile4',
+              text: 'Greek Yogurt',
+              color: 'light',
+            },
+            {
+              type: 'textTile',
+              elementKey: 'textTile5',
+              text: 'Strawberry Yogurt',
+              color: 'light',
+            },
+            {
+              type: 'textTile',
+              elementKey: 'textTile6',
+              text: 'Vanilla Yogurt',
+              color: 'light',
+            },
+            {
+              type: 'textTile',
+              elementKey: 'textTile7',
+              text: 'Blueberry Yogurt',
+              color: 'light',
+            },
+          ],
+        },
+        {
+          type: 'buttonTile',
+          elementKey: 'buttonTile1',
+          color: `light`,
+          text: `Buy Now`,
+        },
+      ],
+    },
+  };
+
+  let renderElement = (element: ElementsType): JSX.Element => {
+    switch (element.type) {
+      case 'imageTile':
+        return (
+          <div className="imageTileContainer" key={element.elementKey}>
+            <img
+              src={element.source}
+              alt={element.title}
+              className="imageTileImage"
+            />
+          </div>
+        );
+      case 'textTile':
+        return (
+          <div
+            className={`textTileContainer ${element.color || ''}`}
+            key={element.elementKey}
+          >
+            <h1 className="header">{element.title}</h1>
+            <p className="textTileText">{element.text}</p>
+          </div>
+        );
+      case 'buttonTile':
+        return (
+          <div className="buttonTileContainer" key={element.elementKey}>
+            {/* Added an anchor tag with the desired URL */}
+            <a
+              href="https://www.carrefour.pl/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <button className="buttonTileButton">{element.text}</button>
+            </a>
+          </div>
+        );
+      case 'horizontalSplitter':
+        return (
+          <div className="horizontalSplitterContainer" key={element.elementKey}>
+            {element.elements.map(renderElement)}
+          </div>
+        );
+      case 'verticalSplitter':
+        return (
+          <div className="verticalSplitterContainer" key={element.elementKey}>
+            {element.elements.map(renderElement)}
+          </div>
+        );
+    }
+  };
+
+  return <div>{renderElement(layout.rootElement)}</div>;
+};
+
+export default App;
---
+++ README.md
@@ -0,0 +1,3 @@
+# My-first-react
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/template-dt-ts-frozsn)
