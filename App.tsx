1a2f7353761b8cb148443286874bf7d5e068303b
Iretomi <114882292+Iretomi@users.noreply.github.com>
2023-04-25 13:50:35 +0200

    importing filles

--- App.tsx
+++ App.tsx
@@ -1,5 +1,5 @@
-import React from "react";
-import "./style.css";
+import React from 'react';
+import './style.css';
 import {
   Layout,
   ElementsType,
@@ -10,80 +10,80 @@ import {
   VerticalSplitter,
   Action,
   Color,
-} from "./model";
+} from './model'; //import all resources from model.ts
 
 let App: React.FC = () => {
   let layout: Layout = {
     rootElement: {
-      type: "verticalSplitter",
+      type: 'verticalSplitter',
       elements: [
         {
-          type: "textTile",
-          elementKey: "textTile1",
-          title: "I Love Yogurt",
+          type: 'textTile',
+          elementKey: 'textTile1',
+          title: 'I Love Yogurt',
 
-          color: "dark",
+          color: 'dark',
         },
         {
-          type: "imageTile",
-          elementKey: "imageTile1",
-          title: "my-yogo",
+          type: 'imageTile',
+          elementKey: 'imageTile1',
+          title: 'my-yogo',
           source:
-            "http://4.bp.blogspot.com/-e1Jz6sKoYK4/UzmmPT4xuQI/AAAAAAAABDw/uCm_kkog4HQ/s1600/parfait+10jpg",
+            'http://4.bp.blogspot.com/-e1Jz6sKoYK4/UzmmPT4xuQI/AAAAAAAABDw/uCm_kkog4HQ/s1600/parfait+10jpg',
         },
 
         {
-          type: "horizontalSplitter",
-          elementKey: "middle",
+          type: 'horizontalSplitter',
+          elementKey: 'middle',
           elements: [
             {
-              type: "textTile",
-              elementKey: "textTile2",
-              title: "Why?",
-              text: "Yogurt is a delicious and healthy treat that I enjoy!. It provides protein and calcium, and it may enhance healthy gut bacteria. Health benefits range from protecting against osteoporosis to relieving irritable bowel disease and aiding digestion, but these depend on the type of yogurt consumed.",
-              color: "mid",
+              type: 'textTile',
+              elementKey: 'textTile2',
+              title: 'Why?',
+              text: 'Yogurt is a delicious and healthy treat that I enjoy!. It provides protein and calcium, and it may enhance healthy gut bacteria. Health benefits range from protecting against osteoporosis to relieving irritable bowel disease and aiding digestion, but these depend on the type of yogurt consumed.',
+              color: 'mid',
             },
           ],
         },
         {
-          type: "verticalSplitter",
-          elementKey: "right",
+          type: 'verticalSplitter',
+          elementKey: 'right',
           elements: [
             {
-              type: "textTile",
-              elementKey: "textTile3",
-              title: "Yogurt Types",
-              color: "light",
+              type: 'textTile',
+              elementKey: 'textTile3',
+              title: 'Yogurt Types',
+              color: 'light',
             },
             {
-              type: "textTile",
-              elementKey: "textTile4",
-              text: "Greek Yogurt",
-              color: "light",
+              type: 'textTile',
+              elementKey: 'textTile4',
+              text: 'Greek Yogurt',
+              color: 'light',
             },
             {
-              type: "textTile",
-              elementKey: "textTile5",
-              text: "Strawberry Yogurt",
-              color: "light",
+              type: 'textTile',
+              elementKey: 'textTile5',
+              text: 'Strawberry Yogurt',
+              color: 'light',
             },
             {
-              type: "textTile",
-              elementKey: "textTile6",
-              text: "Vanilla Yogurt",
-              color: "light",
+              type: 'textTile',
+              elementKey: 'textTile6',
+              text: 'Vanilla Yogurt',
+              color: 'light',
             },
             {
-              type: "textTile",
-              elementKey: "textTile7",
-              text: "Blueberry Yogurt",
-              color: "light",
+              type: 'textTile',
+              elementKey: 'textTile7',
+              text: 'Blueberry Yogurt',
+              color: 'light',
             },
           ],
         },
         {
-          type: "buttonTile",
-          elementKey: "buttonTile1",
+          type: 'buttonTile',
+          elementKey: 'buttonTile1',
           color: `light`,
           text: `Buy Now`,
         },
@@ -93,7 +93,7 @@ let App: React.FC = () => {
 
   let renderElement = (element: ElementsType): JSX.Element => {
     switch (element.type) {
-      case "imageTile":
+      case 'imageTile':
         return (
           <div className="imageTileContainer" key={element.elementKey}>
             <img
@@ -103,17 +103,17 @@ let App: React.FC = () => {
             />
           </div>
         );
-      case "textTile":
+      case 'textTile':
         return (
           <div
-            className={`textTileContainer ${element.color || ""}`}
+            className={`textTileContainer ${element.color || ''}`}
             key={element.elementKey}
           >
             <h1 className="header">{element.title}</h1>
             <p className="textTileText">{element.text}</p>
           </div>
         );
-      case "buttonTile":
+      case 'buttonTile':
         return (
           <div className="buttonTileContainer" key={element.elementKey}>
             {/* Added an anchor tag with the desired URL */}
@@ -126,13 +126,13 @@ let App: React.FC = () => {
             </a>
           </div>
         );
-      case "horizontalSplitter":
+      case 'horizontalSplitter':
         return (
           <div className="horizontalSplitterContainer" key={element.elementKey}>
             {element.elements.map(renderElement)}
           </div>
         );
-      case "verticalSplitter":
+      case 'verticalSplitter':
         return (
           <div className="verticalSplitterContainer" key={element.elementKey}>
             {element.elements.map(renderElement)}
